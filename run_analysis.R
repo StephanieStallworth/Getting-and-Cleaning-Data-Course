b31dcfb76a5a529597956ae0294befed210ed490
StephanieStallworth <scstallworth1@gmail.com>
2017-04-14 07:14:16 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,6 @@
 #Merge the training and the test sets to create one data set.
 
 #Creating directory
-
 if(!file.exists("./data")){dir.create("./data")}
 fileUrl<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileUrl,destfile = "./data/Dataset.zip")
